97a8b83206ff47b39ccab7d698c581843604e889
feveregg <wk45@afsconnect2.njit.edu>
2017-10-19 15:36:10 -0400

    set autoload

--- index.php
+++ index.php
@@ -4,4 +4,18 @@
    error_reporting(E_ALL);
 
 
+   class Manage {	 
+
+    public static function autoload($class) {
+	
+        include $class . '.php';
+	
+	}
+    
+   }
+
+   spl_autoload_register(array('Manage', 'autoload'));
+
+
+
 ?>
