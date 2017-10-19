829d703a108085bd96df05e2d8abd12d363efcae
feveregg <wk45@afsconnect2.njit.edu>
2017-10-19 19:59:43 -0400

    upload form

--- uploadForm.php
+++ uploadForm.php
@@ -1,4 +1,16 @@
 <?php
 
+   class uploadForm extends page {
+
+    	public function get() {
+
+    		$form = '<form action="index.php?page=uploadForm" method="POST" enctype="multipart/form-data">';
+            $form .= '<input type="file" name="fileToUpload" id="fileToUpload">';
+            $form .= '<input type="submit" value="Upload" name="submit">';
+            $form .= '</form>';
+            $this->html .= htmlTags::headingOne("Upload CSV file");
+            $this->html .= $form;
+    	
+    	}
 
 ?>
