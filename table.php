ad9b916205b37491cf7f733e59741ce5ee802e47
feveregg <wk45@afsconnect2.njit.edu>
2017-10-19 23:32:21 -0400

    table starts

---
+++ table.php
@@ -0,0 +1,48 @@
+<?php
+
+    class table extends page {
+
+    	public function get() {
+
+    		
+            
+            chdir('uploads');                                     
+            
+            $file = fopen($_GET['filename'], "r");
+            
+            htmlTags::tableStart();               
+            
+            $row = 1;
+            
+            while (($data=fgetcsv($file)) !== FALSE){    
+            
+                foreach($data as $value) {
+            
+                    if ($row == 1) {
+            
+                        htmlTags::tableHeader($value);
+            
+                    }else {
+             
+                        htmlTags::tableContent($value);
+             
+                    }
+             
+                }
+             
+                $row++;
+                htmlTags::breakTableRow();
+           
+            }
+
+            fclose($file);
+    	}
+    
+
+
+
+
+
+
+    }
+?>
