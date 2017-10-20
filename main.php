79009df8fca7738b7600d11eb0e428c1760ba779
feveregg <wk45@afsconnect2.njit.edu>
2017-10-20 00:50:24 -0400

    add content

--- main.php
+++ main.php
@@ -1,6 +1,34 @@
 <?php
 
+class main {
+   
+        public function __construct() {
 
+        $pageRequest = "uploadForm";
+    
+        if(isset($_REQUEST['page'])) {
+
+        	$pageRequest = $_REQUEST["page"];
+
+        }
+        
+        
+
+        $page = new $pageRequest;
+
+        if($_SERVER["REQUEST_METHOD"] == "GET") {
+
+        	$page->get();
+
+        } else {
+
+        	$page->post();
+
+        }
+
+    }
+
+   }
 
 
 ?>
